0dba7dc45afc3c271893f9cd93f2d353b27eddbf
Christian Kirschberg <ckirschberg@gmail.com>
2022-09-19 11:31:51 +0200

    added comment

--- public/index.php
+++ public/index.php
@@ -1,6 +1,9 @@
 <?php 
 require "./../.env";
 
+
+// What is an inner join?
+
 header("Access-Control-Allow-Origin: *"); 
 header("Content-Type: application/json; charset=UTF-8"); 
 header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE"); 
